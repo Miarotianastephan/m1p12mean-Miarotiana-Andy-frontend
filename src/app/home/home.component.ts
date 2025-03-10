2df4b65b794dab3ba104d083c7de5df543c2b02f
Miarotiana Stephan <miarotianastephan01@gmail.com>
2025-03-11 00:54:24 +0300

    Initial commit done !

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-home',
+  imports: [ToastModule, ButtonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  providers: [MessageService]
+})
+export class HomeComponent {
+  title_home = "Page Accueil"
+  constructor(private messageService: MessageService) {}
+
+  show() {
+      this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Message Content', life: 3000 });
+  }
+}
