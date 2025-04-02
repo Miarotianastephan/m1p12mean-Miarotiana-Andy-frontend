027e59523ae5da353692762f4a8f0b0091cc086c
Miarotiana Stephan <miarotianastephan01@gmail.com>
2025-04-02 11:17:25 +0300

    correction de l'url de l'API

--- src/app/api/axios-config.ts
+++ src/app/api/axios-config.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5000';
-// const BASE_URL = 'https://m1p12mean-andy-miarotiana-backend.onrender.com';
+// const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://m1p12mean-andy-miarotiana-backend.onrender.com';
 const instanceAxios = axios.create({ baseURL: BASE_URL });
 export default instanceAxios;
